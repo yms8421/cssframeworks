cb82dbf22b65706b41264c8cba200ece2248f2e4
yms8421 <yms8421@outlook.com>
2019-06-29 10:37:17 +0300

    initial

---
+++ BilgeAdam.NorthwindPages/BilgeAdam.NorthwindPages/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BilgeAdam.NorthwindPages.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
